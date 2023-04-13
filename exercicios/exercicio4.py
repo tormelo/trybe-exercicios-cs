5e529ed5278204dfded0fc319f345dd113840e6e
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-13 10:11:39 -0300

    2.2: add Ex 4 solution

---
+++ exercicios/exercicio4.py
@@ -0,0 +1,52 @@
+from collections.abc import Iterator, Iterable
+
+
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __repr__(self):
+        return "<%s de %s>" % (self.valor, self.naipe)
+
+
+class IteradorDoBaralho(Iterator):
+    def __init__(self, cartas, estrategia):
+        self._cartas = cartas
+        self._estrategia = estrategia
+        self._pos = 0 if estrategia == 1 else -1
+
+    def __next__(self):
+        try:
+            carta = self._cartas[self._pos]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._pos += self._estrategia
+            return carta
+
+
+class Baralho(Iterable):
+    naipes = "copas ouros espadas paus".split()
+    valores = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+
+    def __init__(self, estrategia):
+        self._cartas = [
+            Carta(valor, naipe)
+            for naipe in self.naipes
+            for valor in self.valores
+        ]
+        self.estrategia = estrategia
+
+    def __len__(self):
+        return len(self._cartas)
+
+    def __iter__(self):
+        return IteradorDoBaralho(self._cartas, self.estrategia)
+
+    def __str__(self):
+        return f"{[carta for carta in self]}"
+
+
+baralho_regular = Baralho(1)
+baralho_inverso = Baralho(-1)
