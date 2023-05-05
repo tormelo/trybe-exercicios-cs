031e3929e365f456cacafa83ec9e45248c74b9d5
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-05 19:00:19 -0300

    5.5 - add Ex 4 solution

---
+++ exercicios/limited_stack.py
@@ -0,0 +1,30 @@
+from stack import Stack
+
+
+class StackOverflow(Exception):
+    pass
+
+
+class LimitedStack(Stack):
+    def __init__(self, limit):
+        super().__init__()
+        self._limit = limit
+
+    def push(self, value):
+        if len(self._data) + 1 > self._limit:
+            raise StackOverflow
+        return super().push(value)
+
+
+if __name__ == "__main__":
+    stack = LimitedStack(2)
+
+    stack.push(1)
+    stack.push(-2)
+
+    print(stack)
+
+    try:
+        stack.push(3)
+    except StackOverflow:
+        print("Não é possível adicionar outro item à pilha")
