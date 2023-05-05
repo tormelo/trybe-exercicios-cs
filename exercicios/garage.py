586c3f835c9db2bdb1f8bda310f629261d15b249
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-05 19:21:36 -0300

    5.5 - add Ex 5 solution

---
+++ exercicios/garage.py
@@ -0,0 +1,40 @@
+from stack import Stack
+
+garage = Stack()
+
+while True:
+    print("Operations: ")
+    print("1. Add car")
+    print("2. Remove a car")
+    print("3. List cars")
+    print("4. Close")
+    option = int(input("Option: "))
+
+    if option == 1:
+        license_plate = input("License plate: ")
+        garage.push(license_plate)
+        print("Car " + license_plate + " parked")
+    elif option == 2:
+        car_to_remove = input("Car to remove (license plate): ")
+        street = Stack()
+        removed = False
+
+        while not garage.is_empty():
+            current_car = garage.pop()
+
+            if current_car == car_to_remove:
+                print("Car " + car_to_remove + " removed")
+                removed = True
+                break
+
+            street.push(current_car)
+
+        while not street.is_empty():
+            garage.push(street.pop())
+
+        if not removed:
+            print("There is no car parked with this plate")
+    elif option == 3:
+        print("Parked vehicles: " + str(garage))
+    else:
+        break
