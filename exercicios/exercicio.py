02c30e54f4e960aef07f5303c62f92fc6af5df64
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-11 19:27:22 -0300

    6.1 - add follow-up 3 solution

--- exercicios/exercicio.py
+++ exercicios/exercicio.py
@@ -3,29 +3,41 @@
 O método add_person possui complexidade O(1) já que realiza uma
 inserção em um hashmap
 
+O método get_score possui complexidade O(1) já que realiza uma
+busca em um hashmap
+
 """
 
 
 class Hierarchy:
     def __init__(self, k):
         self.subordinates = {}
-        # self.scores = {}
+        self.imediate_boss = {}
+        self.scores = {}
         self.k = k
+
+    def update_bosses_scores(self, boss):
+        self.scores[boss] += 1
+        if boss in self.imediate_boss:
+            self.update_bosses_scores(self.imediate_boss[boss])
 
     def add_person(self, boss, person):
         if not boss:
             self.subordinates[person] = []
+            self.scores[person] = 1
             return
 
         if len(self.subordinates[boss]) < self.k:
+            self.imediate_boss[person] = boss
             self.subordinates[boss].append(person)
             self.subordinates[person] = []
-
+            self.scores[person] = 1
+            self.update_bosses_scores(boss)
         else:
             self.add_person(self.subordinates[boss][0], person)
 
-    # def get_score(self, person):
-    #     return self.scores[person]
+    def get_score(self, person):
+        return self.scores[person]
 
 
 if __name__ == "__main__":
@@ -37,9 +49,6 @@ if __name__ == "__main__":
     hierarchy.add_person(4, 5)
     hierarchy.add_person(4, 6)
     hierarchy.add_person(5, 7)
-    print(f"Subordinates: {hierarchy.subordinates}")
 
-    hierarchy.add_person(4, 8)
-    hierarchy.add_person(4, 9)
-    print("\nApós novas inserções")
     print(f"Subordinates: {hierarchy.subordinates}")
+    print(f"Scores: {hierarchy.scores}")
